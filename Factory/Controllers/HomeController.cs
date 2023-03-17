3fd5552c1840c8877bee68205870d3ae4562ad15
E Luckie <eluckie.d@gmail.com>
2023-03-17 09:04:55 -0400

    add Index path & view to HomeController

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,13 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly FactoryContext _db;
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
     [HttpGet("/")]
     public ActionResult Index()
     {
+      List<Engineer> engineers = _db.Engineers.ToList();
+      List<Machine> machines = _db.Machines.ToList();
+      ViewBag.engineers = engineers;
+      ViewBag.machines = machines;
       return View();
     }
   }
